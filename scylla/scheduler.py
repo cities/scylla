70b7050abe23498727bcb87bbcaeefc9a3925ad5
lmwang9527 <lmwang@gmail.com>
2021-05-12 01:40:29 +0000

    only use anonymous proxy for server; fix a minor bug in logger.error

--- scylla/scheduler.py
+++ scylla/scheduler.py
@@ -32,7 +32,7 @@ def fetch_ips(q: Queue, validator_queue: Queue):
                 try:
                     html = worker.get_html(url, render_js=provider.should_render_js())
                 except Exception as e:
-                    logger.error("worker.get_html failed: ", e)
+                    logger.exception(e)
                     continue
 
                 if html:
